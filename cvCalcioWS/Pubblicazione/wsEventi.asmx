df88e4ecc0409a2926407744a5975d69394b9aaa
Luigi Pecce <looigi@gmail.com>
2019-08-15 21:58:20 +0200

    Gestione Eventi

---
+++ cvCalcioWS/Pubblicazione/wsEventi.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="vb" CodeBehind="wsEventi.asmx.vb" Class="cvCalcioWS.wsEventi" %>
---
+++ cvCalcioWS/wsEventi.asmx.vb
@@ -0,0 +1,140 @@
+﻿Imports System.Web.Services
+Imports System.Web.Services.Protocols
+Imports System.ComponentModel
+
+<System.Web.Services.WebService(Namespace:="http://cvcalcio_dir.org/")>
+<System.Web.Services.WebServiceBinding(ConformsTo:=WsiProfiles.BasicProfile1_1)>
+<ToolboxItem(False)>
+Public Class wsEventi
+	Inherits System.Web.Services.WebService
+
+	<WebMethod()>
+	Public Function SalvaEvento(idEvento As String, Evento As String) As String
+		Dim Ritorno As String = ""
+		Dim Connessione As String = LeggeImpostazioniDiBase(Server.MapPath("."))
+
+		If Connessione = "" Then
+			Ritorno = ErroreConnessioneNonValida
+		Else
+			Dim Conn As Object = ApreDB(Connessione)
+
+			If TypeOf (Conn) Is String Then
+				Ritorno = ErroreConnessioneDBNonValida & ":" & Conn
+			Else
+				Dim Rec As Object = Server.CreateObject("ADODB.Recordset")
+				Dim Sql As String = ""
+				Dim idEve As Integer = -1
+
+				If idEvento = -1 Then
+					Try
+						Sql = "SELECT Max(idEvento)+1 FROM Eventi"
+						Rec = LeggeQuery(Conn, Sql, Connessione)
+						If TypeOf (Rec) Is String Then
+							Ritorno = Rec
+						Else
+							If Rec(0).Value Is DBNull.Value Then
+								idEve = 1
+							Else
+								idEve = Rec(0).Value
+							End If
+							Rec.Close()
+						End If
+					Catch ex As Exception
+						Ritorno = StringaErrore & " " & ex.Message
+					End Try
+				Else
+					idEve = idEvento
+					Sql = "Delete * From Eventi Where idEvento=" & idEve
+					Ritorno = EsegueSql(Conn, Sql, Connessione)
+				End If
+
+				Sql = "Insert Into Eventi Values (" &
+					" " & idEve & ", " &
+					"'" & Evento.Replace("'", "''") & "', " &
+					"'N'" &
+					")"
+				Ritorno = EsegueSql(Conn, Sql, Connessione)
+
+				Conn.Close()
+			End If
+		End If
+
+		Return Ritorno
+	End Function
+
+	<WebMethod()>
+	Public Function RitornaEventi() As String
+		Dim Ritorno As String = ""
+		Dim Connessione As String = LeggeImpostazioniDiBase(Server.MapPath("."))
+
+		If Connessione = "" Then
+			Ritorno = ErroreConnessioneNonValida
+		Else
+			Dim Conn As Object = ApreDB(Connessione)
+
+			If TypeOf (Conn) Is String Then
+				Ritorno = ErroreConnessioneDBNonValida & ":" & Conn
+			Else
+				Dim Rec As Object = Server.CreateObject("ADODB.Recordset")
+				Dim Sql As String = ""
+
+				Try
+					Sql = "SELECT * From Eventi Where Eliminato='N' Order By idEvento"
+					Rec = LeggeQuery(Conn, Sql, Connessione)
+					If TypeOf (Rec) Is String Then
+						Ritorno = Rec
+					Else
+						If Rec.Eof Then
+							Ritorno = StringaErrore & " No events found"
+						Else
+							Ritorno = ""
+							Do Until Rec.Eof
+								Ritorno &= Rec("idEvento").Value.ToString & ";" & Rec("Descrizione").Value.ToString & "§"
+
+								Rec.MoveNext()
+							Loop
+						End If
+						Rec.Close()
+					End If
+				Catch ex As Exception
+					Ritorno = StringaErrore & " " & ex.Message
+				End Try
+
+				Conn.Close()
+			End If
+		End If
+
+		Return Ritorno
+	End Function
+
+	<WebMethod()>
+	Public Function EliminaEvento(idEvento As String) As String
+		Dim Ritorno As String = ""
+		Dim Connessione As String = LeggeImpostazioniDiBase(Server.MapPath("."))
+
+		If Connessione = "" Then
+			Ritorno = ErroreConnessioneNonValida & ":" & Connessione
+		Else
+			Dim Conn As Object = ApreDB(Connessione)
+
+			If TypeOf (Conn) Is String Then
+				Ritorno = ErroreConnessioneDBNonValida & ":" & Conn
+			Else
+				Dim Sql As String = ""
+				Dim Ok As Boolean = True
+
+				Try
+					Sql = "Update Eventi Set Eliminato='S' Where idEvento=" & idEvento
+					Ritorno = EsegueSql(Conn, Sql, Connessione)
+				Catch ex As Exception
+					Ritorno = StringaErrore & " " & ex.Message
+					Ok = False
+				End Try
+
+				Conn.Close()
+			End If
+		End If
+
+		Return Ritorno
+	End Function
+End Class
